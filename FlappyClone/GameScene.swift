62c68bc0a8f7bd17afc801feaf8fd920ab95ed87
Josh Sachs <josh_sachs_weldre5j@re5j-ipad.local>
2016-01-27 19:40:39 -0700

    Initial Commit

--- FlappyClone/GameScene.swift
+++ FlappyClone/GameScene.swift
@@ -6,40 +6,170 @@
 //  Copyright (c) 2016 Josh Sachs. All rights reserved.
 //
 
+import GameplayKit
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+        createPlayer()
+        createSky()
+        createBackground()
+        createGround()
+        initRocks()
+        createScore()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func createPlayer() {
+        let playerTexture = SKTexture(imageNamed: "player-1")
+        player = SKSpriteNode(texture: playerTexture)
+        player.zPosition = 10
+        player.position = CGPoint(x: frame.width / 6, y: frame.height * 0.75)
+        addChild(player)
+        
+        let frame2 = SKTexture(imageNamed: "player-2")
+        let frame3 = SKTexture(imageNamed: "player-3")
+        let animation = SKAction.animateWithTextures([playerTexture, frame2, frame3, frame2], timePerFrame: 0.01)
+        let runForever = SKAction.repeatActionForever(animation)
+        
+        player.runAction(runForever)
+    }
+    
+    func createSky() {
+        let topSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.14, brightness: 0.97, alpha: 1), size: CGSize(width: frame.width, height: frame.height * 0.67))
+        topSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+        
+        let bottomSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.16, brightness: 0.96, alpha: 1), size: CGSize(width: frame.width, height: frame.height * 0.33))
+        topSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+        
+        topSky.position = CGPoint(x: CGRectGetMidX(frame), y: frame.size.height)
+        bottomSky.position = CGPoint(x: CGRectGetMidX(frame), y: bottomSky.frame.height / 2)
+        
+        addChild(topSky)
+        addChild(bottomSky)
+        
+        bottomSky.zPosition = -40
+        topSky.zPosition = -40
+    }
+    
+    func createBackground() {
+        let backgroundTexture = SKTexture(imageNamed: "background")
+        
+        for i in 0 ... 1 {
+            let background = SKSpriteNode(texture: backgroundTexture)
+            background.zPosition = -30
+            background.anchorPoint = CGPointZero
+            background.position = CGPoint(x: (backgroundTexture.size().width * CGFloat(i)) - CGFloat(1 * i), y: 100)
+            addChild(background)
+            
+            let moveLeft = SKAction.moveByX((-backgroundTexture.size().width), y: 0, duration: 20)
+            let moveReset = SKAction.moveByX(backgroundTexture.size().width, y: 0, duration: 0)
+            let moveLoop = SKAction.sequence([moveLeft, moveReset])
+            let move4Eva = SKAction.repeatActionForever(moveLoop)
+            
+            background.runAction(move4Eva)
+        }
+    }
+    
+    func createGround() {
+        let groundTexture = SKTexture(imageNamed: "ground")
+        
+        for i in 0 ... 1 {
+            let ground = SKSpriteNode(texture: groundTexture)
+            ground.zPosition = -10
+            ground.position = CGPoint(x: (groundTexture.size().width / 2.0 + (groundTexture.size().width * CGFloat(i))), y: groundTexture.size().height / 2)
+            addChild(ground)
+            
+            let moveLeft = SKAction.moveByX(-groundTexture.size().width, y: 0, duration: 5)
+            let moveReset = SKAction.moveByX(groundTexture.size().width, y: 0, duration: 0)
+            let moveLoop = SKAction.sequence([moveLeft, moveReset])
+            let move4eva = SKAction.repeatActionForever(moveLoop)
+            ground.runAction(move4eva)
+        }
+    }
+    
+    func createRocks() {
+        let rockTexture = SKTexture(imageNamed: "rock")
+        
+        let topRock = SKSpriteNode(texture: rockTexture)
+        topRock.zRotation = CGFloat(M_PI)
+        topRock.xScale = -1.0
+        
+        let bottomRock = SKSpriteNode(texture: rockTexture)
+        
+        topRock.zPosition = -20
+        bottomRock.zPosition = -20
+        
+        let rockCollision = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: 32, height: frame.height))
+        rockCollision.name = "scoreDetect"
+
+        addChild(topRock)
+        addChild(bottomRock)
+        addChild(rockCollision)
+        
+        let xPosition = frame.width + topRock.frame.width
+        
+        let max = Int(frame.height / 3)
+        let rand = GKRandomDistribution(lowestValue: -100, highestValue: max)
+        let yPosition = CGFloat(rand.nextInt())
+        
+        // This value affects the width of the the gap between the rocks.
+        let rockDistance: CGFloat = 70
+        
+        topRock.position = CGPoint(x: xPosition, y: yPosition + topRock.size.height + rockDistance)
+        bottomRock.position = CGPoint(x: xPosition, y: yPosition - rockDistance)
+        rockCollision.position = CGPoint(x: xPosition + (rockCollision.size.width * 2), y: CGRectGetMidY(frame))
+        
+        let endPosition = frame.width + (topRock.frame.width * 2)
+        
+        let moveAction = SKAction.moveByX(-endPosition, y: 0, duration: 5.8)
+        let moveSequence = SKAction.sequence([moveAction, SKAction.removeFromParent()])
+        topRock.runAction(moveSequence)
+        bottomRock.runAction(moveSequence)
+        rockCollision.runAction(moveSequence)
+        
+    }
+    
+    func initRocks() {
+        let create = SKAction.runBlock { [unowned self] in
+            self.createRocks()
+        }
+        
+        let wait = SKAction.waitForDuration(3)
+        let sequence = SKAction.sequence([create, wait])
+        let repeat4Eva = SKAction.repeatActionForever(sequence)
+        
+        runAction(repeat4Eva)
+    }
+    
+    func createScore() {
+        scoreLabel = SKLabelNode(fontNamed: "Optima-ExtraBlack")
+        scoreLabel.fontSize = 24
+        
+        scoreLabel.position = CGPointMake(CGRectGetMaxX(frame) - 20, CGRectGetMaxY(frame) - 40)
+        scoreLabel.horizontalAlignmentMode = .Right
+        scoreLabel.text = "Score: 0"
+        scoreLabel.fontColor = UIColor.blackColor()
+        addChild(scoreLabel)
     }
 }
